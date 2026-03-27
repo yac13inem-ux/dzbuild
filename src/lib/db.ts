2cff3a0c0359e30ccfffcf5b2d4221fd88bcbf33
Z User <z@container>
2026-03-27 14:57:46 +0000

    build: trigger Vercel rebuild

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -41,3 +41,4 @@ export interface GuestComment {
 export default supabase
 // Force redeploy Fri Mar 20 22:44:08 UTC 2026
 // Build trigger Sat Mar 21 09:36:38 UTC 2026
+// Build trigger Fri Mar 27 14:57:46 UTC 2026
